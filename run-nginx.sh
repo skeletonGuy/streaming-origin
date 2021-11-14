53487d236beb51e403411580142bb5c06980b462
Ken Reiswig <kreiswig@swagg-book-bro.local>
2021-11-14 11:11:32 -0800

    add x86 version of Docker image

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 CONTAINER_NAME=nginx_rtmp_origin
 IMAGE_NAME=nginx_rtmp
+ARM=false
 
-echo "Building nginx-rtmp image"
-docker build -t $IMAGE_NAME .
+while getopts a flag
 
-docker run -it --name $CONTAINER_NAME -p 8080:8080 -p 1935:1935 $IMAGE_NAME
+do
+    case "${flag}" in
+        a) ARM=true
+    esac
+done
+
+# ARM option available for local dev on macs with m1 chip
+if [ $ARM == "true" ] 
+then
+    echo "Building nginx-rtmp ARM image"
+    docker build -t $IMAGE_NAME .
+    echo "Starting nginx-rtmp ARM image"
+    docker run -it --name $CONTAINER_NAME -p 8080:8080 -p 1935:1935 $IMAGE_NAME
+else
+    echo "Building nginx-rtmp x86 image"
+    docker build -t $IMAGE_NAME -f Dockerfile-x86 .
+    echo "Starting nginx-rtmp x86 image"
+    docker run -it --name $CONTAINER_NAME -p 8080:8080 -p 1935:1935 $IMAGE_NAME
+fi
